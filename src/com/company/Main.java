9a615d9ae14215facc166308041453cddaefa23f
Jose Dominguez <jadominguez99@gmail.com>
2020-09-16 12:22:58 -0400

    Creation of the method sumFirstAndLastDigit

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        sumFirstAndLastDigit(125);
+    }
+    public static int sumFirstAndLastDigit(int number) {
+
+        int sum = 0;
+        if(number < 0 ) {
+            return -1;
+        }
+        int lastDigit = number % 10;
+
+        while (number > 0 ){
+
+            int firstDigit = number % 10;
+            number /= 10;
+            sum = firstDigit + lastDigit;
+
+        }
+        System.out.println(sum);
+        return sum;
+    }
+}
